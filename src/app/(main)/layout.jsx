8d00723721fcda0be86d883e8641ff8b6da673e5
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-04-25 17:34:26 +0600

    Project Init set up

---
+++ src/app/(main)/layout.jsx
@@ -0,0 +1,14 @@
+import Header from '@/components/shared/Header';
+import Navber from '@/components/shared/Navber';
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <>
+        <Header />
+        <Navber />
+        </>
+    );
+};
+
+export default MainLayout;
