0229c7f4299d7f6183f648c8a41409f9b2b8df93
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-04-29 10:36:59 +0600

    url ishhu fixwd

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,9 +1,7 @@
 import { createAuthClient } from "better-auth/react"
-
 export const authClient = createAuthClient({
-    // ১. প্রোডাকশন ইউআরএল (URL-এর শেষে / দেবেন না)
-    baseURL: "https://dragon-news-eight-sigma.vercel.app" 
+    /** The base URL of the server (optional if you're using the same domain) */
+    baseURL: "https://dragon-news-eight-sigma.vercel.app"
 })
 
-// ২. আগে তৈরি করা 'authClient' থেকেই ফাংশনগুলো এক্সপোর্ট করুন
-export const { signIn, signUp, useSession } = authClient;
+export const { signIn, signUp, useSession } = createAuthClient()
